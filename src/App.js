21099f11bb73ed2a043736bce3eee45b0f991d75
Amol Mahale <mahalea1000@gmail.com>
2025-01-27 19:06:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddStudent from './Components/AddStudent';
 
 function App() {
